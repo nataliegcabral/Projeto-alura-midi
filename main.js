f6efc8617d5407f4127c8487c67f2afcb5ed7ecb
Natalie Cabral <nataliegc23@gmail.com>
2022-11-26 16:07:11 -0300

    update

---
+++ main.js
@@ -0,0 +1,13 @@
+function soundUh() {
+    document.querySelector('#som_tecla_uh').play();
+}
+
+document.querySelectorAll('.tecla').onclick = soundUh;
+
+//a função é executada assim para evitar código inline e para evitar repetições de código
+
+// listaDeTeclas
+
+
+
+
