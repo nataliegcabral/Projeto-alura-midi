7208f98948fb7bb7df44fa69dd58fea886591f4e
Natalie Cabral <nataliegc23@gmail.com>
2022-11-27 16:03:52 -0300

    update

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-function soundUh() {
-    document.querySelector('#som_tecla_pom').play();
+function sound(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+   const idAudio = `#som_${instrumento}`;
+
+
+    tecla.onclick = function() {
+        sound(idAudio)
+    }
+
+}
 
 
 
